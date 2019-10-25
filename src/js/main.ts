44d0baf034346a5960069c69b6aab30dea975ea2
Mikihiro SUDA <su.glasses.923@gmail.com>
2019-10-25 16:24:49 +0900

    add: show paper title

--- src/js/main.ts
+++ src/js/main.ts
@@ -1,9 +1,30 @@
-console.log('hoge')
-console.log('cool!')
+import axios from "axios";
+import { Paper } from "../models";
 
-const dom = document.createElement('div');
-dom.textContent = 'GOOD!';
+window.addEventListener("DOMContentLoaded", () => {
+  axios.get("/mock.json").then(res => {
+    const papers = res.data as Paper[];
+    papers.forEach(paper => {
+      // Element Elementを生成
+      const paperElement = document.createElement("div");
+      const titleElement = document.createElement("div");
+      const urlElement = document.createElement("div");
 
-window.addEventListener('DOMContentLoaded', () => {
-    document.body.appendChild(dom);
-})
+      // Elementにクラスを適用
+      paperElement.classList.add("paper");
+      titleElement.classList.add("paper--title");
+      urlElement.classList.add("paper--url");
+
+      // Elementにテキストを挿入
+      titleElement.textContent = paper.title;
+      urlElement.textContent = paper.url;
+
+      // 子Elementをpaper Elementに挿入
+      paperElement.appendChild(titleElement);
+      paperElement.appendChild(urlElement);
+
+      // bodyにpaper elementを挿入
+      document.body.appendChild(paperElement);
+    });
+  });
+});
---
+++ src/models/index.ts
@@ -0,0 +1,18 @@
+export interface Authhor {
+  id: string;
+  name: string;
+}
+
+export interface Keyword {
+  id: string;
+  name: string;
+}
+
+export interface Paper {
+  abstract: string;
+  title: string;
+  authors: Authhor[];
+  publishedAt: string;
+  keywords: Keyword[];
+  url: string;
+}
