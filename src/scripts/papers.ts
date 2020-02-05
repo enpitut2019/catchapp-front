858ec926efe9a93bd4da50bf9d6231fe9ce73822
Aika-Murakami <aika.murakami@yourmystar.jp>
2020-02-05 13:37:51 +0900

    検索結果のtitleとdescriptionを動的に変更

--- src/scripts/papers.ts
+++ src/scripts/papers.ts
@@ -35,6 +35,12 @@ const appendPapers = (papers: Paper[]): void => {
   papers.forEach((paper, idx) => {
     if (paperTemplate === null) return;
 
+    // titleとdescriptionの設定
+    document.title = paperNameRaw + " の検索結果 | CatchApp";
+    document
+      .querySelector("meta[name='description']")!
+      .setAttribute("content", paperNameRaw + " の検索結果 \n CatchAppで「" + paperNameRaw + "」に関する論文をサクサク見つけよう。");
+
     // Elementを生成
     const paperElement = document.importNode(paperTemplate.content, true);
 
